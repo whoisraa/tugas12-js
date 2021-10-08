73dc14b31340525399f8faee166344795e2b84ed
whoisraa <raainvictus@gmail.com>
2021-10-08 18:43:26 +0700

    tugas 12 js

---
+++ tugas12.js
@@ -0,0 +1,12 @@
+function callObject() {
+    let buah = ['Pisang', 'Jeruk', 'Apel', 'Mangga'];
+    console.log(buah);
+
+    buah.pop();
+    console.log(buah);
+
+    buah.shift();
+    console.log(buah);
+}
+
+callObject();
